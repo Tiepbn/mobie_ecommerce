82a0a34b2a35036d4b70718f2a42c92ebeee0ca3
Tiep-14 <conho3841@gmail.com>
2024-03-27 19:53:18 +0700

    a

--- lib/consts/lists.dart
+++ lib/consts/lists.dart
@@ -15,11 +15,11 @@ const secondSlidersList = [imgSs1, imgSs2, imgSs3, imgSs4];
 const featuredImage1 = [imgS1, imgS2, imgS3];
 const featuredImage2 = [imgS4, imgS5, imgS6];
 
-const freaturedTitles1 = [womenDress, girlsDress, girlsWatches];
+const freaturedTitles1 = [toys, girlsDress, girlsWatches];
 const freaturedTitles2 = [boysGlasses, mobilePhone, tShirts];
 
 const categoriesList = [
-  womenDress,
+  girlsDress,
   menClothingFashion,
   compAcess,
   automobile,
